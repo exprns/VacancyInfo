97da27e8f802e407797a25691f29ee0f40588e50
exprns <ububo@yandex.ru>
2020-10-28 21:55:53 +0300

    delete Middle Man methods
    Удалено несколько метод посредников

--- VacancyInfo/Classes/VacancyData.cs
+++ VacancyInfo/Classes/VacancyData.cs
@@ -10,39 +10,14 @@ namespace VacancyInfo.Classes
 {
     public interface IVacancyData
     {
-        Task<List<HHVacancyModel>> GetVacanciesAsync(string name, string region = "");
-        Task<List<HHVacancyModel>> GetVacanciesInDetail();
-        decimal GetAverageSalary();
-        Dictionary<int, decimal> GetRegionsSalaries();
-        decimal GetRegionAverageSalary(int areaId);
+        decimal GetAverageSalary(List<HHVacancyModel> vacancies);
+        Dictionary<int, decimal> GetRegionsSalaries(Dictionary<int, List<HHVacancyModel>> vacanciesByRegionWithSalary);
     }
 
     public class VacancyData : IVacancyData
     {
-        private IVacancyService _vacancyService;
 
-        public VacancyData(IVacancyService vacancyService)
-        {
-            _vacancyService = vacancyService;
-        }
-
-        public async Task<List<HHVacancyModel>> GetVacanciesAsync(string name, string region = "")
-        {
-            await _vacancyService.GetVacancies(name, region);
-            return new List<HHVacancyModel>(); // TODO: думаю тут сделать ответ о том что данные получены и можно строить графики
-        }
-
-        public async Task<List<HHVacancyModel>> GetVacanciesInDetail() => await _vacancyService.GetVacanciesInDetail(); // TODO: думаю что стоит убрать отсюда все ф-и, которые сквозно вызывают функции из ваканси сервиса
-
-        public List<Area> GetAreas() => _vacancyService.Areas;
-        public Dictionary<int, List<HHVacancyModel>> GetVacanciesByRegionWithSalary() => _vacancyService.VacanciesByRegionWithSalary;
-        public Dictionary<int, List<HHVacancyModel>> GetVacanciesByRegion() => _vacancyService.VacanciesByRegion;
-        public decimal GetAverageSalary() => GetAverageSalary(_vacancyService.VacanciesWithSalary);
-        public decimal GetRegionAverageSalary(int areaId) => GetAverageSalary(_vacancyService.VacanciesByRegionWithSalary[areaId]);
-        public List<HHVacancyModel> GetRegionVacancies(int areaId) => _vacancyService.VacanciesByRegion[areaId];
-
-
-        private decimal GetAverageSalary(List<HHVacancyModel> vacancies)
+        public decimal GetAverageSalary(List<HHVacancyModel> vacancies)
         {
             decimal avgFrom = vacancies.Sum(x => x.salary.from.Value) / vacancies.Count;
             decimal avgTo = vacancies.Sum(x => x.salary.to.Value) / vacancies.Count;
@@ -50,12 +25,12 @@ namespace VacancyInfo.Classes
             return (avgFrom + avgTo) / 2;
         }
 
-        public Dictionary<int, decimal> GetRegionsSalaries()
+        public Dictionary<int, decimal> GetRegionsSalaries(Dictionary<int, List<HHVacancyModel>> vacanciesByRegionWithSalary)
         {
             Dictionary<int, decimal> avgSalaryByReg = new Dictionary<int, decimal>();
-            foreach(var regionVacancies in _vacancyService.VacanciesByRegionWithSalary)
+            foreach(var regionVacancies in vacanciesByRegionWithSalary)
             {
-                avgSalaryByReg.Add(regionVacancies.Key, GetRegionAverageSalary(regionVacancies.Key));
+                avgSalaryByReg.Add(regionVacancies.Key, GetAverageSalary(regionVacancies.Value));
             }
             return avgSalaryByReg;
         }
--- VacancyInfo/Controllers/VacancyController.cs
+++ VacancyInfo/Controllers/VacancyController.cs
@@ -15,39 +15,42 @@ namespace VacancyInfo.Controllers
     {
         private VacancyData _vacancyData;
         private IJsonService _jsonService;
+        private IVacancyService _vacancyService;
+
 
         public VacancyController(IVacancyService vacancyService, IJsonService jsonService)
         {
-            _vacancyData = new VacancyData(vacancyService);
+            _vacancyData = new VacancyData();
+            _vacancyService = vacancyService;
             _jsonService = jsonService;
         }
 
         // GET api/Vacancy/Vacancies?name=5
         [HttpGet("Vacancies")]
-        public async Task<List<HHVacancyModel>> GetVacanciesAsync(string name)
+        public async Task<List<HHVacancyModel>> GetVacanciesAsync(string name, string region = "")
         {
-            return await _vacancyData.GetVacanciesAsync(name); // TODO: думаю тут сделать ответ о том что данные получены и можно строить графики
+            return await _vacancyService.GetVacancies(name, region); // TODO: думаю тут сделать ответ о том что данные получены и можно строить графики
         }
 
         // GET api/Vacancy/VacanciesInDetail
         [HttpGet("VacanciesInDetail")]
         public async Task<List<HHVacancyModel>> GetVacanciesInDetailAsync()
         {
-            return await _vacancyData.GetVacanciesInDetail();
+            return await _vacancyService.GetVacanciesInDetail(); ;
         }
 
         // GET api/Vacancy/GetAvarageSalary
         [HttpGet("GetAvarageSalary",Name ="getAvarageSalary")]
         public decimal GetAverageSalary()
         {
-            return _vacancyData.GetAverageSalary();
+            return _vacancyData.GetAverageSalary(_vacancyService.VacanciesWithSalary);
         }
 
         // GET api/Vacancy/GetAreasJson
         [HttpGet("GetAreasJson", Name = "getAreasJson")]
         public string GetAreasJson()
         {
-            var areas = _vacancyData.GetAreas();
+            var areas = _vacancyService.Areas;
             return _jsonService.JsonSerializeAllUnicode(areas);
         }
 
@@ -55,8 +58,8 @@ namespace VacancyInfo.Controllers
         [HttpGet("GetAverageRegionSalary", Name = "getAverageRegionSalary")]
         public decimal GetAverageRegionSalary(int areaId)
         {
-            var areas = _vacancyData.GetAreas();
-            var regSalaries = _vacancyData.GetRegionAverageSalary(areaId);
+            var areas = _vacancyService.Areas;
+            var regSalaries = _vacancyData.GetAverageSalary(_vacancyService.VacanciesByRegionWithSalary[areaId]);
             return regSalaries;
         }
 
@@ -64,7 +67,7 @@ namespace VacancyInfo.Controllers
         [HttpGet("GetRegionVacanciesJson", Name = "getRegionVacanciesJson")]
         public string GetRegionVacanciesJson(int areaId)
         {
-            var vacs = _vacancyData.GetRegionVacancies(areaId);
+            var vacs = _vacancyService.VacanciesByRegion[areaId];
             return _jsonService.JsonSerializeAllUnicode(vacs);
         }
 
